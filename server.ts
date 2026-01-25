83ab69000e1bf1c033fbea9e164ff3117f3fabcc
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-25 15:33:28 +0530

    added db url

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import ENV from "./utils/envVariable";
 
-const PORT = ENV.port;
+const PORT = ENV.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);

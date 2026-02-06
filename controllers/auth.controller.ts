bde4fdfbb5358e251c8aadfb9c8941f90124705c
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-02-06 19:20:16 +0530

    same site configuration change

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -21,7 +21,7 @@ export const login = catchAsync(async (req: Request, res: Response) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 

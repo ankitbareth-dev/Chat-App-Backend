0808b98bed5de02c818c456825c7ce9d7c1040e9
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-02-25 11:35:05 +0530

    feat(db): add type and duration fields to Message model

--- prisma/migrations/20260220041914_add_message_seen_status/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN     "seenAt" TIMESTAMP(3);
--- prisma/migrations/20260222051904_add_message_composite_index/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "Message_receiverId_senderId_seenAt_idx" ON "Message"("receiverId", "senderId", "seenAt");

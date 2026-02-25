375afde9b3998886bcf5bd63b41e53bb237de8be
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-02-25 11:45:11 +0530

    feat(config): add multer configuration for audio file uploads

--- config/multer.ts
+++ config/multer.ts
@@ -3,7 +3,7 @@ import { AppError } from "../utils/AppError";
 
 const storage = multer.memoryStorage();
 
-const fileFilter: multer.Options["fileFilter"] = (req, file, cb) => {
+const imageFileFilter: multer.Options["fileFilter"] = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
@@ -11,12 +11,26 @@ const fileFilter: multer.Options["fileFilter"] = (req, file, cb) => {
   }
 };
 
-const limits = {
-  fileSize: 2 * 1024 * 1024,
+const audioFileFilter: multer.Options["fileFilter"] = (req, file, cb) => {
+  if (file.mimetype.startsWith("audio")) {
+    cb(null, true);
+  } else {
+    cb(new AppError(400, "Only audio files are allowed!"));
+  }
 };
 
 export const upload = multer({
   storage,
-  fileFilter,
-  limits,
+  fileFilter: imageFileFilter,
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+});
+
+export const uploadVoice = multer({
+  storage,
+  fileFilter: audioFileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
